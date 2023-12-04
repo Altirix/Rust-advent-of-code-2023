41372c27fd62deec8f110edcf2e73e44062595d8
ALtirix <19439141+Altirix@users.noreply.github.com>
2023-12-04 02:49:47 +0000

    Code cleanup

--- day-01/src/bin/part1.rs
+++ day-01/src/bin/part1.rs
@@ -1,4 +1,3 @@
-
 fn main() {
     let input = include_str!("input1.txt");
     let output = part1(input);
@@ -6,33 +5,30 @@ fn main() {
 }
 
 fn part1(input: &str) -> u32 {
-
-    let result:u32 = input
+    let result: u32 = input
         .lines()
-        .map( |line| {
-
+        .map(|line| {
             let digits: Vec<u32> = line
                 .chars()
-                .filter_map( |_char| _char.to_digit(10)).collect();
+                .filter_map(|_char| _char.to_digit(10))
+                .collect();
 
-            let line_result: u32 = digits.first().unwrap_or_else(|| &0) * 10
-                                 + digits.last().unwrap_or_else(|| &0);
+            let line_result: u32 =
+                digits.first().unwrap_or_else(|| &0) * 10 + digits.last().unwrap_or_else(|| &0);
 
             return line_result;
-                        
-    }).sum::<u32>();
-
+        })
+        .sum::<u32>();
 
     return result;
 }
 
-
 #[cfg(test)]
-mod tests { 
+mod tests {
     use super::*;
 
     #[test]
-    fn valid_answer(){
+    fn valid_answer() {
         let input = "1abc2
 pqr3stu8vwx
 a1b2c3d4e5f
--- day-01/src/bin/part2.rs
+++ day-01/src/bin/part2.rs
@@ -1,4 +1,3 @@
-
 fn main() {
     let input = include_str!("input1.txt");
     let output = part2(input);
@@ -6,51 +5,51 @@ fn main() {
 }
 
 fn part2(input: &str) -> u32 {
+    static WORDS: [(&str, u32); 9] = [
+        ("one", 1),
+        ("two", 2),
+        ("three", 3),
+        ("four", 4),
+        ("five", 5),
+        ("six", 6),
+        ("seven", 7),
+        ("eight", 8),
+        ("nine", 9),
+    ];
 
-    static WORDS: [(&str,u32); 9] = [("one",1), ("two",2), ("three",3), ("four",4),("five",5), ("six",6),("seven",7), ("eight",8), ("nine",9)];
-
-    let result:u32 = input
+    let result: u32 = input
         .lines()
-        .map( |line| {
-            
-            let digits: Vec<u32> = (0.. line.len())
-                .filter_map(| index|  { 
+        .map(|line| {
+            let digits: Vec<u32> = (0..line.len())
+                .filter_map(|index| {
                     let sub_line = &line[index..];
 
-                   return WORDS
-                        .iter()
-                        .find_map( |word| 
-                            if sub_line.starts_with(word.0) {
-                                return Some(word.1);
-                            } else {
-                                return sub_line
-                                    .chars()
-                                    .next()
-                                    .unwrap()
-                                    .to_digit(10);});
-                }).collect();
+                    return WORDS.iter().find_map(|word| {
+                        if sub_line.starts_with(word.0) {
+                            return Some(word.1);
+                        } else {
+                            return sub_line.chars().next().unwrap().to_digit(10);
+                        }
+                    });
+                })
+                .collect();
 
-
-            let line_result: u32 = digits.first().unwrap_or_else(|| &0) * 10
-                                 + digits.last().unwrap_or_else(|| &0);
-
-
+            let line_result: u32 =
+                digits.first().unwrap_or_else(|| &0) * 10 + digits.last().unwrap_or_else(|| &0);
 
             return line_result;
-                        
-    }).sum::<u32>();
-
+        })
+        .sum::<u32>();
 
     return result;
 }
 
-
 #[cfg(test)]
-mod tests { 
+mod tests {
     use super::*;
 
     #[test]
-    fn valid_answer(){
+    fn valid_answer() {
         let input = "two1nine
         eightwothree
         abcone2threexyz
