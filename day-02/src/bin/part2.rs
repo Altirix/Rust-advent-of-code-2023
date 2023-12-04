b274d36a71aff4abc69787241a1f92f8884672ab
ALtirix <19439141+Altirix@users.noreply.github.com>
2023-12-04 12:59:12 +0000

    revert some silly testing

--- day-02/src/bin/part2.rs
+++ day-02/src/bin/part2.rs
@@ -70,7 +70,7 @@ fn part2(input: &str) -> u32 {
     return games
         .iter()
         .map(|game| {
-            game.rounds.iter().fold(BTreeMap::new(), |acc: BTreeMap<Colour, u32>, set| {
+            game.rounds.iter().fold(BTreeMap::new(), |acc, set| {
                 let red: (Colour, u32) = (
                     Colour::Red,
                     *set.get(&Colour::Red)
@@ -106,10 +106,14 @@ mod tests {
 
     #[test]
     fn valid_answer() {
-        let input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green";
+        let input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+        Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+        Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+        Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+        Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
 
         let result = part2(input);
 
-        assert_eq!(result, 2285)
+        assert_eq!(result, 2286)
     }
 }
